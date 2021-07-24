ca4403a3c89c32fc3aa09d507a5c780622a596de
Sven Mattsen <sven@sven.cc>
2021-07-24 12:10:04 +0200

    remove complicated automation key

--- k3s/get_kubeconfig.sh
+++ k3s/get_kubeconfig.sh
@@ -14,21 +14,16 @@ function check_deps() {
 
 function parse_input() {
   # jq reads from stdin so we don't have to set up any inputs, but let's validate the outputs
-  eval "$(jq -r '@sh "export CLUSTER_IP=\(.cluster_ip) PRIVKEY=\(.private_key)"')"
+  eval "$(jq -r '@sh "export CLUSTER_IP=\(.cluster_ip)"')"
   [[ -n "${CLUSTER_IP}" ]] || error_exit "cluster_ip not found"
-  [[ -n "${PRIVKEY}" ]] || error_exit "private_key not found"
 }
 
 function get_kubeconfig() {
-  trap 'rm -rf "${tmp_dir}"' EXIT
-  tmp_dir="$(mktemp -d)"
-  (umask 77 && echo "${PRIVKEY}" | base64 -d > "${tmp_dir}/key")
-  #while ! kubeconfig="$(SSH_AUTH_SOCK= ssh -o StrictHostKeyChecking=no -o GlobalKnownHostsFile=/dev/null -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes -F /dev/null -i \"${tmp_dir}/key\" -l root \"${CLUSTER_IP}\" \"while ! [ -f /etc/rancher/k3s/k3s.yaml ] || ! cat /etc/rancher/k3s/k3s.yaml; do sleep 5; done\")"; do sleep 5; done
-  while ! kubeconfig="$(SSH_AUTH_SOCK= ssh -o PasswordAuthentication=no -o StrictHostKeyChecking=no -o GlobalKnownHostsFile=/dev/null -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes -F /dev/null -i "${tmp_dir}/key" -l root ${CLUSTER_IP} "while ! [ -f /etc/rancher/k3s/k3s.yaml ] || ! cat /etc/rancher/k3s/k3s.yaml; do sleep 5; done")"; do sleep 5; done
+  while ! kubeconfig="$(ssh -o PasswordAuthentication=no -o StrictHostKeyChecking=no -o GlobalKnownHostsFile=/dev/null -o UserKnownHostsFile=/dev/null -F /dev/null -l root ${CLUSTER_IP} "while ! [ -f /etc/rancher/k3s/k3s.yaml ] || ! cat /etc/rancher/k3s/k3s.yaml; do sleep 5; done")"; do sleep 5; done
   kubeconfig="${kubeconfig/127\.0\.0\.1/${CLUSTER_IP}}"
   server="$(echo "${kubeconfig}" | grep server | cut -d : -f 2-)"
   while [ "$(curl --output /dev/null --silent --head --fail --write-out '%{http_code}' -k ${server})x" != "401x" ]; do sleep 5; done
-  jq -n --arg kubeconfig "${kubeconfig}" '{"kubeconfig":$kubeconfig}'
+  jq -n --arg kubeconfig "${kubeconfig}" '{"kubeconfig":$kubeconfig, "kubeconfig_base64":$kubeconfig | @base64}'
 }
 
 
--- k3s/main.tf
+++ k3s/main.tf
@@ -10,21 +10,15 @@ resource "hcloud_server" "master" {
   backups = true
   ssh_keys = var.ssh_keys
   user_data = templatefile("${path.module}/server_userdata.tmpl", {
-    automation_key = data.external.ssh_key_generator.result.public_key
     extra_ssh_keys = var.extra_ssh_keys,
     k3s_version = var.k3s_version
   })
-}
-
-data "external" "ssh_key_generator" {
-  program = ["bash", "${path.module}/ssh_key_generator.sh"]
 }
 
 data "external" "get_kubeconfig" {
   program = ["bash", "${path.module}/get_kubeconfig.sh"]
 
   query = {
-    private_key = data.external.ssh_key_generator.result.private_key_base64
     cluster_ip = hcloud_server.master.ipv4_address
   }
 }
