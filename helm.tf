a40f730b1d64ab6c986ebec1fc162f38b0eb0e9c
Sven Mattsen <sven@sven.cc>
2021-07-26 00:24:24 +0200

    providers

--- helm.tf
+++ helm.tf
@@ -1,8 +1,8 @@
-resource "helm_release" "openebs" {
-  name       = "openebs"
-  repository = "https://openebs.github.io/charts"
-  chart      = "openebs"
-  version    = "2.11.2"
-  namespace  = "openebs"
-  create_namespace = true
-}
+#resource "helm_release" "openebs" {
+#  name       = "openebs"
+#  repository = "https://openebs.github.io/charts"
+#  chart      = "openebs"
+#  version    = "2.11.2"
+#  namespace  = "openebs"
+#  create_namespace = true
+#}
--- provider.tf
+++ provider.tf
@@ -8,6 +8,22 @@ terraform {
       source = "hashicorp/helm"
       version = ">= 2.2.0"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = ">= 2.3.2"
+    }
+    kubernetes-alpha = {
+      source = "hashicorp/kubernetes-alpha"
+      version = ">= 0.5.0"
+    }
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = ">= 1.11.2"
+    }
+    http = {
+      source = "hashicorp/http"
+      version = ">= 2.1.0"
+    }
   }
 }
 
@@ -24,4 +40,25 @@ provider "helm" {
     client_key = base64decode(module.masterpool.key)
     cluster_ca_certificate = base64decode(module.masterpool.ca)
   }
+}
+
+provider "kubernetes" {
+  host = module.masterpool.host
+  client_certificate = base64decode(module.masterpool.cert)
+  client_key = base64decode(module.masterpool.key)
+  cluster_ca_certificate = base64decode(module.masterpool.ca)
+}
+
+provider "kubernetes-alpha" {
+  host = module.masterpool.host
+  client_certificate = base64decode(module.masterpool.cert)
+  client_key = base64decode(module.masterpool.key)
+  cluster_ca_certificate = base64decode(module.masterpool.ca)
+}
+
+provider "kubectl" {
+  host = module.masterpool.host
+  client_certificate = base64decode(module.masterpool.cert)
+  client_key = base64decode(module.masterpool.key)
+  cluster_ca_certificate = base64decode(module.masterpool.ca)
 }
